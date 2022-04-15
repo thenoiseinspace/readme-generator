b45afa59ce5c3d6124fa5b8f0cf5a293062c7721
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-14 22:03:02 -0400

    setting up variables for user content to write to page

--- assets/generator.js
+++ assets/generator.js
@@ -1,3 +1,23 @@
-//template literal: 
-//const greeting = `My name is ${arya.first}!
-//I am loyal to ${arya.allegiance}.`;
+//response.main, 
+
+// const arya = {
+//     first: "Arya",
+//     last: "Stark",
+//     origin: "Winterfell",
+//     allegiance: "House Stark"
+//   };
+  
+//   const greeting = `My name is ${arya.first}!
+//   I am loyal to ${arya.allegiance}.`;
+  
+//   console.log(greeting); // prints
+  // My name is Arya!
+  // I am loyal to House Stark.
+
+const finalTitle = `Project Title:\n ${response.title}`
+const finalDescription = `Project Description:\n ${response.description}`
+const finalInstallation = `Project Installation: \n ${response.installation}`
+const finalUsage = `Project Usage: \n ${response.usage}`
+const finalCredits = `Project Credits: \n ${response.credits}`
+const finalLicense = `Project License: \n ${response.license}`
+
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 const inquirer = require('inquirer');
+//linking fs here allows us to write to a file
 const fs = require('fs');
+//imports the other js file to generate the page
 const generator = require("./generator");
 
-
+//prompting user for answers
+//this is based on activity 19
 inquirer
   .prompt([
     {
@@ -40,5 +43,27 @@ inquirer
     console.log(response) 
     //console logging individual part to check
     // console.log(response.username)
+
+    //it's breaking--is this a scopiing problem? 
   }
   );
+
+  //Making the write to file in this file but grabbing data from generator file
+  //generator page is a bunch of functions that have template literals
+
+   //This section comes from the Node documentation 
+
+const content = {
+  finalTitle, finalDescription, finalInstallation, finalUsage, finalCredits
+}; 
+
+//this generates a folder called Output and a file called generated-readme. It prints the content to the readme and puts that file in the folder. 
+ fs.writeFile('/Output/generated-readme.md', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})
+
+
