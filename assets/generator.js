87209b532754382b2502446c0eac808a6f779bc5
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-14 21:24:48 -0400

    adding psuedocode, gettng responses from user

---
+++ assets/generator.js
@@ -0,0 +1,3 @@
+//template literal: 
+//const greeting = `My name is ${arya.first}!
+//I am loyal to ${arya.allegiance}.`;
--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generator = require("./generator");
 
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your name?',
-      name: 'username',
+      message: 'What is your project called?',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'What languages do you know?',
-      name: 'languages',
+      message: 'Briefly describe your project.',
+      name: 'description',
     },
     {
       type: 'input',
-      message: 'Preferred method of communication',
-      name: 'comms',
+      message: 'What steps are required to install and run your project?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How will your project be used?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Did you collaborate with anyone or use any external resources to build this project?',
+      name: 'credits',
+    },
+    {
+      type: 'input',
+      message: 'What license governs the usage of your project?',
+      name: 'license',
     },
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('It did not work')
+  .then((response) => {
+    console.log(response) 
+    //console logging individual part to check
+    // console.log(response.username)
+  }
   );
